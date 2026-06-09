d024e8e98d067e204e44af42007007953099b245
aaron <aaroncuesho19@gmail.com>
2026-06-08 22:28:56 -0600

    feat: generate member-list and member-form components

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'FitFlow Lite - Gestión';
+  vistaActual: 'lista' | 'formulario' = 'lista'; // Controlador de vistas
+
+  cambiarVista(vista: 'lista' | 'formulario') {
+    this.vistaActual = vista;
+  }
+}
